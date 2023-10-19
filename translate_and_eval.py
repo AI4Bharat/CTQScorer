c473c9d12a1a79998ee95c1a4e74ad3f8c101b9e
Ashwanth <kumaraswanth@gmail.com>
2023-10-20 00:22:15 +0530

    update translate and eval file

--- translate_and_eval.py
+++ translate_and_eval.py
@@ -48,31 +48,6 @@ from scoring_functions import get_chrf_scores, get_comet_scores, get_comet_mean_
 from helper_functions import read_recommendations, get_samples, clear_gpu_memory, get_model
 
 # %% [markdown]
-# ### Constants
-
-# %%
-BATCH_SIZE = 8
-
-RANKINGS_LABSE = 'rankings_labse'
-RANKINGS_BM25 = 'rankings_bm25'
-RANKINGS_BM25_AND_RERANKING = 'rankings_bm25_and_reranking'
-RANKINGS_LABSE_AND_RERANKING = 'rankings_labse_and_reranking'
-RANKINGS_KNN_ROBERTA = 'rankings_knn_roberta'
-RANKINGS_COMET_QA = 'rankings_comet_qa'
-RANKINGS_BM25_AND_LABSE = 'rankings_bm25_and_labse'
-RANKINGS_BM25_AND_CHRF = 'rankings_bm25_and_chrf'
-RANKINGS_BM25_AND_3_WAY = 'rankings_bm25_and_3_way'
-RANKINGS_BM25_AND_3_WAY_ORACLE = 'rankings_bm25_and_3_way_oracle'
-RANKINGS_BM25_REGRESSION = 'rankings_bm25_regression'
-RANKINGS_BM25_AND_DIVERSITY = 'rankings_bm25_and_diversity'
-RANKINGS_REGRESSION = 'rankings_regression'
-RANDOM_SELECTION = 'random_selection'
-
-# Constants related to Dataset
-SAMANANTAR = 'samanantar'
-FLORES = 'flores'
-
-# %% [markdown]
 # ### Initiating Scoring functions
 
 # %%
@@ -82,10 +57,28 @@ comet_qe_20_metric = init_comet_qe_20_computation()
 comet_da_22_metric = init_comet_da_22_computation()
 
 # %% [markdown]
-# ### Function to generate MT and evaluating translation
+# ### Functions to generate MT and evaluating translation
 
 # %%
-# This function evaluates the BLOOM model and also captures the MT outputs
+def update_nested_strategy_based_on_strategy(mp: model_parameters):
+    if mp.strategy in [COMET_QE_QUERY_SRC_SCORE, COMET_QE_QUERY_DST_SCORE, COMET_QE_SRC_DST_SCORE]:
+        mp.strategy_nested = mp.strategy
+        mp.strategy = RANKINGS_COMET_QA
+    elif mp.strategy in [LABSE_SCORE_QUERY_SRC, LABSE_SCORE_QUERY_DST, LABSE_SCORE_SRC_DST]:
+        mp.strategy_nested = mp.strategy
+        mp.strategy = RANKINGS_BM25_AND_3_WAY
+    elif mp.strategy in [SRC_DST_PPL, SRC_DST_QUERY_PPL]:
+        mp.strategy_nested = mp.strategy
+        mp.strategy = RANKINGS_BM25_AND_PERPLEXITY
+    elif mp.strategy in [NO_OF_TOKENS_IN_SRC_SENT, NO_OF_TOKENS_IN_DST_SENT]:
+        mp.strategy_nested = mp.strategy
+        mp.strategy = RANKINGS_NO_OF_TOKENS
+    return mp
+
+# %%
+
+# This function generates MT and evaluates the translation using the model 
+# specified in model parameters and also applies ranking strategy.
 def get_bleu_scores(pipe, mp: model_parameters, experiment=''):
     model_name = mp.name.split('/')[1]
     
@@ -98,6 +91,9 @@ def get_bleu_scores(pipe, mp: model_parameters, experiment=''):
     make_dir(output_dir)
     make_dir(prompts_dir)
 
+    # updating nested strategy helps to read from right recommendation file
+    mp = update_nested_strategy_based_on_strategy(mp)
+
     # make note of configuration
     scores_file = '{}/scores.csv'.format(output_dir)
     msg = '{} [{}]\n'.format(str(mp).strip(), experiment)
@@ -109,7 +105,7 @@ def get_bleu_scores(pipe, mp: model_parameters, experiment=''):
 
     # get ranking of dev samples if reranking flag is true
     if mp.has_reranking:
-        rankings = read_recommendations(mp.strategy, mp.training_source, mp.testing_source, mp.src_lang, mp.dst_lang, mp.strategy_nested)
+        rankings = read_recommendations(mp.strategy, mp.training_source, mp.testing_source, mp.src_lang, mp.dst_lang)
         if len(rankings) == 0:
             print('No ranking found for: {}'.format(src_lang))
             return
@@ -130,51 +126,37 @@ def get_bleu_scores(pipe, mp: model_parameters, experiment=''):
         if mp.has_reranking:
             recommendations = rankings[str(qid)]
             
-            # # COMET_QE_QUERY_DST_SCORE, COMET_QE_SRC_DST_SCORE
-            # if mp.strategy_nested == COMET_QE_QUERY_DST_SCORE \
-            # or mp.strategy_nested == COMET_QE_SRC_DST_SCORE:
-            #     recommendations.sort(key=lambda x: x[mp.strategy_nested], reverse=True)
-            #     recommendations = list(map(lambda x: x["index"], recommendations))
-            #     # print(recommendations)
-            # elif mp.strategy_nested == SRC_DST_PPL\
-            # or mp.strategy_nested == SRC_DST_QUERY_PPL:
-            #     recommendations.sort(key=lambda x: x[mp.strategy_nested])
-            #     recommendations = list(map(lambda x: x["index"], recommendations))
-            #     # print(recommendations)
-            # elif mp.strategy_nested == NO_OF_TOKENS_IN_SRC_SENT \
-            # or mp.strategy_nested == NO_OF_TOKENS_IN_DST_SENT:
-            #     recommendations.sort(key=lambda x: x[mp.strategy_nested], reverse=True)
-            #     recommendations = list(map(lambda x: x["index"], recommendations))
-
-            # recommendation structure has been changed
-            if mp.strategy == RANKINGS_BM25 \
-            or mp.strategy == RANKINGS_LABSE \
-            or mp.strategy == RANKINGS_COMET_QA \
-            or mp.strategy == RANKINGS_BM25_AND_LABSE \
-            or mp.strategy == RANKINGS_BM25_AND_LABSE_QUERY_DST \
-            or mp.strategy == RANKINGS_BM25_AND_LABSE_SRC_DST \
-            or mp.strategy == RANKINGS_BM25_AND_CHRF \
-            or mp.strategy == RANKINGS_BM25_AND_3_WAY \
-            or mp.strategy == RANKINGS_BM25_AND_3_WAY_ORACLE \
-            or mp.strategy == RANKINGS_REGRESSION \
-            or mp.strategy == RANKINGS_3WAY_REGRESSION \
-            or mp.strategy == RANKINGS_NO_OF_TOKENS \
-            or mp.strategy == RANKINGS_CUSTOM \
-            or mp.strategy == RANKINGS_LINEAR_REGRESSION:
+            # inside nested strategies, there can be multiple different ways to choose reranking            
+            if mp.strategy_nested:
+                if mp.strategy_nested in [COMET_QE_QUERY_SRC_SCORE, COMET_QE_QUERY_DST_SCORE, COMET_QE_SRC_DST_SCORE]:
+                    recommendations.sort(key=lambda x: x[mp.strategy_nested], reverse=True)
+                elif mp.strategy_nested in [LABSE_SCORE_QUERY_SRC, LABSE_SCORE_QUERY_DST, LABSE_SCORE_SRC_DST]:
+                    recommendations.sort(key=lambda x: x[mp.strategy_nested], reverse=True)
+                elif mp.strategy_nested in [SRC_DST_PPL, SRC_DST_QUERY_PPL]:
+                    recommendations.sort(key=lambda x: x[mp.strategy_nested])
+                elif mp.strategy_nested in [NO_OF_TOKENS_IN_SRC_SENT, NO_OF_TOKENS_IN_DST_SENT]:
+                    recommendations.sort(key=lambda x: x[mp.strategy_nested], reverse=True)
+                recommendations = list(map(lambda x: x["index"], recommendations))
+            elif mp.strategy == RANKINGS_BM25_AND_RERANKING:
+                pass 
+            elif mp.strategy in [RANKINGS_BM25, RANKINGS_BM25_AND_CHRF, RANKINGS_BM25_AND_3_WAY, 
+                                 RANKINGS_3WAY_REGRESSION, RANKINGS_REGRESSION, RANKINGS_LINEAR_REGRESSION]:
                 # recommendations are in [{ "index": 630729, "score": 37.21}, ... ]
                 recommendations = list(map(lambda x: x["index"], recommendations))
+            else:
+                print('Invalid strategy: {}'.format(mp.strategy))
+                return
 
             # tries to take different prompt examples
             if mp.diversify_prompts:
                 recommendations = recommendations[0::10]
 
-            # Remove the repetitive prompts. Also smaller sentences are present ahead of bigger sentences 
-            # in BM25 ranking. 
-            # if mp.training_source == SAMANANTAR:
+            # Remove the repetitive examples
             recommendations = handle_repetitive_examples(src_train_samples, dst_train_samples, recommendations)
             
             # take recommendations as many as the no of shots
             recommendations = recommendations[:mp.no_of_shots]
+            
             # changes the order of prompts (low-score to high-score examples)
             if mp.inc_reranking:
                 recommendations.reverse()
@@ -216,12 +198,10 @@ def get_bleu_scores(pipe, mp: model_parameters, experiment=''):
     comet_qe_20_score = round(np.mean(comet_qe_20_scores), 4)
     print('comet_qe_20_score score -> {}'.format(comet_qe_20_score))
 
-
     comet_da_22_scores = get_comet_da_22_scores(predicted=pred_dst, references=dst_test_samples, source=src_test_samples, comet_da_22_metric=comet_da_22_score)
     comet_da_22_scores = list(map(lambda x: round(x, 4), comet_da_22_scores))
     comet_da_22_score = round(np.mean(comet_da_22_scores), 4)
     print('comet_da_22_score score -> {}'.format(comet_da_22_score))
-
     
     # obtain chrf and chrf++ score
     chrf_score, chrfpp_score = get_chrf_scores(pred_dst, dst_test_samples, chrf)
@@ -231,7 +211,7 @@ def get_bleu_scores(pipe, mp: model_parameters, experiment=''):
         f.write('{},{},{},{},{},{},{},{},{},{},{},{},{}\n'.format(model_name, mp.type_of_algo, src_lang, dst_lang, mp.no_of_shots, blue_score, comet_score, chrf_score, chrfpp_score, comet_qe_20_score, comet_da_22_score, mp.use_8_bit, random_name))
 
 # %% [markdown]
-# ### Evaluation
+# ### Machine Translation and Evaluation
 
 # %%
 # name = "facebook/opt-6.7b", BLOOM_3B, BLOOM_7B, XGLM_7B
@@ -254,81 +234,11 @@ mp.testing_source=FLORES
 mp.has_reranking=True
 mp.inc_reranking=True
 mp.no_of_shots=4
-
-experiment = 'exp_120_test'
-
 mp.strategy = RANKINGS_BM25
-# mp.strategy_nested = COMET_QE_20_REGRESSION
-
 mp.src_lang=BEN_BENG
 mp.dst_lang=ENG_LATN
+
+experiment = 'exp_120_test'
 get_bleu_scores(pipe, mp, experiment='{}.1'.format(experiment))
-
-# mp.src_lang=GUJ_GUJR
-# mp.dst_lang=ENG_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.2'.format(experiment))
-
-# mp.src_lang=HIN_DEVA
-# mp.dst_lang=ENG_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.3'.format(experiment))
-
-# mp.src_lang=ENG_LATN
-# mp.dst_lang=BEN_BENG
-# get_bleu_scores(pipe, mp, experiment='{}.4'.format(experiment))
-
-# mp.src_lang=ENG_LATN
-# mp.dst_lang=GUJ_GUJR
-# get_bleu_scores(pipe, mp, experiment='{}.5'.format(experiment))
-
-# mp.src_lang=ENG_LATN
-# mp.dst_lang=HIN_DEVA
-# get_bleu_scores(pipe, mp, experiment='{}.6'.format(experiment))
-
-
-
-# %%
-# # get bleu score
-# mp.training_source=EUROPARL
-# mp.testing_source=FLORES
-# mp.has_reranking=True
-# mp.inc_reranking=True
-# mp.no_of_shots=4
-
-# mp.strategy = RANKINGS_NO_OF_TOKENS
-# mp.strategy_nested = NO_OF_TOKENS_IN_SRC_SENT
-
-# mp.src_lang=FRA_LATN
-# mp.dst_lang=ENG_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.7'.format(experiment))
-
-# mp.src_lang=DEU_LATN
-# mp.dst_lang=ENG_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.8'.format(experiment))
-
-# mp.src_lang=ENG_LATN
-# mp.dst_lang=FRA_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.9'.format(experiment))
-
-# mp.src_lang=ENG_LATN
-# mp.dst_lang=DEU_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.10'.format(experiment))
-
-
-# %%
-# # get bleu score
-# mp.training_source=PARACRAWL
-# mp.testing_source=FLORES
-# mp.has_reranking=True
-# mp.inc_reranking=True
-# mp.no_of_shots=4
-
-# mp.src_lang=RUS_CYRL
-# mp.dst_lang=ENG_LATN
-# get_bleu_scores(pipe, mp, experiment='{}.11'.format(experiment))
-
-# mp.src_lang=ENG_LATN
-# mp.dst_lang=RUS_CYRL
-# get_bleu_scores(pipe, mp, experiment='{}.12'.format(experiment))
-
 
 
